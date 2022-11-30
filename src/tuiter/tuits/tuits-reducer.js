0c56863f1b9ac0cba9bd47c5d975ad1f6794b137
Srikar Kokkanti <ks692@snu.edu.in>
2022-11-30 03:47:15 -0500

    Assignment 8

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -59,6 +59,7 @@ const tuitsSlice = createSlice({
         [updateTuitThunk.fulfilled]:
             (state, { payload }) => {
                 state.loading = false
+                console.log(state.tuits)
                 const tuitNdx = state.tuits
                     .findIndex((t) => t._id == payload._id)
                 state.tuits[tuitNdx] = {
