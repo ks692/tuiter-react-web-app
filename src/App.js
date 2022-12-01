0f841d1948a418b410590e617158c74e903978dc
Srikar Kokkanti <ks692@snu.edu.in>
2022-11-30 20:49:18 -0500

    Assignment 9

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="container">
                 <Routes>
                     <Route index
-                           path="/*"element={<Labs/>}/>
+                           path="/"element={<Labs/>}/>
                     <Route path="/hello"
                            element={<HelloWorld/>}/>
                     <Route path="/tuiter/*" element={<Tuiter/>}/>
