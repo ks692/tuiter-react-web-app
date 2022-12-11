5804da37df5c9a035cdacaa98f7102946af01f8a
Srikar Kokkanti <ks692@snu.edu.in>
2022-12-11 18:19:11 -0500

    Server change

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'https://node-server-app-sriki.herokuapp.com/api/tuits';
+const TUITS_API = 'https://node-backend-blb1.onrender.com/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
